410b60da56f1f6b921942cfa6c67776ed1f57cb3
Bennett Robinson <bennett.robinson@techtonic.com>
2021-09-17 15:00:09 -0600

    Skeleton jsx files created for each component

---
+++ src/components/BookForm.jsx
@@ -0,0 +1,9 @@
+const BookForm = () => {
+  return (
+    <div>
+      <h1>Book</h1>
+    </div>
+  );
+};
+
+export default BookForm;
---
+++ src/pages/BookShelf.jsx
@@ -0,0 +1,9 @@
+const BookShelf = () => {
+  return (
+    <div>
+      <h1>Book Sheld</h1>
+    </div>
+  );
+};
+
+export default BookShelf;
