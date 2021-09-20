695f5c814043545194ca635d503a354e842fc32e
Bennett Robinson <bennett.robinson@techtonic.com>
2021-09-20 15:18:23 -0600

    Bookshelf in jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,16 @@ const Home = () => {
           <span className="homepage__info">Read the books if you dare.</span>
         </p>
       </section>
-      <section className="main">
-        <p className="main__title"> Welcome to the Library</p>
-        <p className="main__description">
+      <section className="mainHome">
+        <p className="mainHome__title"> Welcome to the Library</p>
+        <p className="mainHome__description">
           This is the greatest book in the world you should definietly come pick
           it up.
         </p>
         <NavLink to="/bookshelf" className="btn">
           See Books
         </NavLink>
-        <p className="main__description">
+        <p className="mainHome__description">
           This is a very long text that you should look at if you want to know
           what is going on. This project wants me to write some text about some
           books
